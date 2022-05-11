50449b0397314bc41c9fa514bfed3679f03c94dc
MarkTin <91620987+Mark-TinZ@users.noreply.github.com>
2022-05-11 17:12:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+
 from GUI.Stext import Ui_MainWindow
 
+
 class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def critical(self, Error):
+        msgBox = QMessageBox()
+        msgBox.setWindowTitle('Error')
+        msgBox.setIcon(QMessageBox.Critical)
+        msgBox.setText(Error.__doc__)
+        msgBox.setStandardButtons(QMessageBox.Close)
+        msgBox.setDefaultButton(QMessageBox.Close)
+        msgBox.setDetailedText(str(Error))
+        reply = msgBox.exec()
+
+        if reply == QMessageBox.Close:
+            quit()
+
 
 def main():
-	app = QtWidgets.QApplication(sys.argv)
-	window = ExampleApp()
-	window.show()
-	app.exec_()
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    app.exec_()
 
 
 if __name__ == '__main__':
-	main()
+    main()
