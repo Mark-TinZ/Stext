c4adbf5dbcda6ceb8b965f37d48c0863c409a583
mark <mixailkar55@gmail.com>
2022-05-17 12:03:44 +0600

    Adding TODO

--- MainWin.py
+++ MainWin.py
@@ -10,14 +10,17 @@ class Main_window(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         try:
+            # todo Create protection for error understanding and stress tolerance
             self.headerbar = self.menubar
             self.headerbar.triggered[QAction].connect(self.menubarconnect)
         except Exception as Err:
+            # todo Add pop-up windows in which you can conveniently observe the program error
             print(Err.__doc__)
             print(str(Err))
 
     def menubarconnect(self, Obj: Exception):
         print(Obj.objectName())
+        # todo Handling top menu events
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
