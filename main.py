b21c5fcc37c0a5aa02511e8de44d25b833ca4141
mark <mixailkar55@gmail.com>
2022-05-17 11:21:07 +0600

    Init

--- main.py
+++ main.py
@@ -1,37 +1,77 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QAction
 
-from GUI.Stext import Ui_MainWindow
+from GUI.Stext import Ui_MainWindow as MainWin
+from GUI.Stext_Settings import Ui_MainWindow as SettingWin
+import pytesseract
+#pdf = pytesseract.image_to_pdf_or_hocr('test.png', extension='pdf')
+#pytesseract.run_and_get_output()
+class Settings(QtWidgets.QMainWindow, SettingWin):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
-class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
-		try:
-			import pusdsdf
-		except Exception as err:
-			self.critical(err)
+class ExampleApp(QtWidgets.QMainWindow, MainWin):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.settings = None
 
-	def critical(self, Error):
-		msgBox = QMessageBox()
-		msgBox.setWindowTitle('Error')
-		msgBox.setIcon(QMessageBox.Critical)
-		msgBox.setText("test text")
-		msgBox.setStandardButtons(QMessageBox.Retry | QMessageBox.Abort | QMessageBox.Ignore)
-		msgBox.setDefaultButton(QMessageBox.Retry)
-		msgBox.setDetailedText(str(Error))
-		reply = msgBox.exec()
+        try:
+            self.footerbar = self.statusbar
+            self.headerbar = self.menubar
+        except Exception:
+            self.critical(Exception)
 
-		if reply == QMessageBox.Retry:
-			pass
+
+        try:
+            self.footerbar.showMessage("test", 10000)
+            self.headerbar.triggered[QAction].connect(self.menubarconect)
+        except Exception:
+            self.critical(Exception)
+
+    def menubarconect(self, obj):
+        print(obj.objectName())
+        match obj.objectName():
+            case "actionOpen":
+                self.footerbar.showMessage("is clicked", 2000)
+            case "actionSettings":
+                self.openwin_settings()
+            case "actionQuit":
+                self.close()
+            case "actionAbout_Stext":
+                pass
+            case "actionAbout_image_editor":
+                pass
+            case "actionabout_the_program":
+                pass
+
+    def openwin_settings(self):
+        self.settings = Settings()
+        self.settings.show()
+
+    def critical(self, Error: IndexError):
+        msgBox = QMessageBox()
+        msgBox.setWindowTitle('Error')
+        msgBox.setIcon(QMessageBox.Critical)
+        msgBox.setText(Error.__doc__)
+        msgBox.setStandardButtons(QMessageBox.Close)
+        msgBox.setDefaultButton(QMessageBox.Close)
+        msgBox.setDetailedText(str(Error))
+        reply = msgBox.exec()
+
+        if reply == QMessageBox.Close:
+            quit()
+
+
 
 def main():
-	app = QtWidgets.QApplication(sys.argv)
-	window = ExampleApp()
-	window.show()
-	app.exec_()
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    app.exec_()
 
 
 if __name__ == '__main__':
-	main()
+    main()
