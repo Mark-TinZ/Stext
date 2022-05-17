0f6b7b133b2d657339fab276dbf5fad94ddbb394
mark <mixailkar55@gmail.com>
2022-05-17 11:52:13 +0600

    Updating the main program window

--- MainWin.py
+++ MainWin.py
@@ -1,7 +1,26 @@
 import sys
 from PyQt5 import QtWidgets
-
+from PyQt5.QtWidgets import QMessageBox, QAction
+from GUI.Stext import Ui_MainWindow
 
 # main class Stext
-class Main_window(QtWidgets.QMainWindow,):
-    pass
+class Main_window(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        try:
+            self.headerbar = self.menubar
+            self.headerbar.triggered[QAction].connect(self.menubarconnect)
+        except Exception as Err:
+            print(Err.__doc__)
+            print(str(Err))
+
+    def menubarconnect(self, Obj: Exception):
+        print(Obj.objectName())
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Main_window()
+    window.show()
+    app.exec_()
